a84b501e26a723fb31d124cc4efe546fa99081d0
Nick <ScrelliCopter@users.noreply.github.com>
2016-07-11 02:34:49 +1000

    Implemented a basic bot. When you say ping he says pong, how innovative.

---
+++ Bot.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Sass
+{
+	internal class Bot
+	{
+		private DiscordClient m_client;
+
+		public void Start ()
+		{
+			if ( m_client != null )
+			{
+				throw new Exception ( "Bot is already started" );
+			}
+
+			// Load the config file.
+			try
+			{
+				Config.Open ( "config.json" );
+			}
+			catch ( ConfigException e )
+			{
+				Console.WriteLine ( $"Failed to open config.json: {e.Message}" );
+				return;
+            }
+
+			// Create client.
+			m_client = new DiscordClient ( ( conf ) =>
+			{
+				conf.LogLevel = LogSeverity.Info;
+				conf.LogHandler = OnLogHandler;
+			} );
+
+			// Set event handlers.
+			m_client.MessageReceived += OnMessage;
+
+			m_client.ExecuteAndWait ( async () =>
+			{
+				await m_client.Connect ( Config.Instance.Get<string> ( "Token", "" ) );
+				m_client.SetGame ( Config.Instance.Get<string> ( "Game" ) );
+			} );
+		}
+
+		private void OnLogHandler ( object sender, LogMessageEventArgs e )
+		{
+			Console.WriteLine ( "DN[{0}] {1}: {2}", e.Severity, e.Source, e.Message );
+			if ( e.Exception != null )
+			{
+				Console.WriteLine ( "Something done goofed" );
+				Console.WriteLine ( e.Exception );
+			}
+		}
+
+		private void OnMessage ( object sender, MessageEventArgs e )
+		{
+			// Ignore our own messages.
+			if ( e.User.Id == e.Server.CurrentUser.Id )
+			{
+				return;
+			}
+
+			if ( e.Message.RawText == "ping" )
+			{
+				e.Channel.SendMessage ( "pong" );
+			}
+		}
+
+		//TODO: Reimplement these 
+
+		/*
+		private void OnErrorHandler ( object sender, CommandErrorEventArgs e )
+		{
+			string msg = e.Exception?.GetBaseException ().Message;
+
+			// No exception, show generic message.
+			if ( msg == null )
+			{
+				switch ( e.ErrorType )
+				{
+					case ( CommandErrorType.Exception ):
+						msg = "Idk what the fuck happened, but this message should not be shown unless something really went shit side up.";
+						break;
+					case ( CommandErrorType.BadPermissions ):
+						msg = "You don't have permission to run this ya fuggin druggo.";
+						break;
+					case ( CommandErrorType.BadArgCount ):
+						msg = "Wrong number of arguments you silly galah.";
+						break;
+
+					default:
+						msg = null;
+						break;
+				}
+
+				if ( msg != null )
+				{
+					e.Channel.SendMessage ( msg );
+				}
+			}
+			else
+			{
+				e.Channel.SendMessage ( "Something goofed behind the scenes, blame Nu's shit code." );
+				m_client.Log.Error ( "Command", e.Exception );
+			}
+		}
+
+		private void OnCommandExecuted ( object sender, CommandEventArgs e )
+		{
+			Console.WriteLine ( "{1} called by {0}", e.User.Name, e.Command.Text );
+		}
+		*/
+	}
+}
---
+++ Config.cs
@@ -0,0 +1,122 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Sass
+{
+	public class ConfigException : System.Exception
+	{
+		public ConfigException ( string a_message ) : base ( a_message ) {}
+	}
+
+	class Config
+	{
+		// Singleton shit.
+		private static Config m_instance = null;
+		public static Config Instance
+		{
+			get { return m_instance; }
+		}
+
+		// Members.
+		Dictionary<string, object> m_tokens;
+		
+		// Opan doooor.
+		private string GenPrefix ( Stack<string> a_objStack )
+		{
+			if ( a_objStack.Count == 0 )
+			{
+				return "";
+			}
+
+			StringBuilder sb = new StringBuilder ();
+			foreach ( string obj in a_objStack )
+			{
+				sb.Append ( obj );
+				sb.Append ( '.' );
+			}
+			return sb.ToString ();
+		}
+
+		private void _Open ( string a_path )
+		{
+			JsonTextReader reader = null;
+			try
+			{
+				reader = new JsonTextReader ( new StreamReader ( a_path ) );
+			}
+			catch ( IOException e )
+			{
+				throw new ConfigException ( $"IO error: {e.Message}" );
+			}
+
+			m_tokens = new Dictionary<string, object> ();
+			string token = null;
+			string objPrefix = "";
+			Stack<string> objStack = new Stack<string> ();
+
+			while ( reader.Read () )
+			{
+				switch ( reader.TokenType )
+				{
+				case ( JsonToken.StartObject ):
+					if ( reader.Value != null )
+					{
+						objStack.Push ( reader.Value as string );
+						objPrefix = GenPrefix ( objStack );
+					}
+					break;
+
+				case ( JsonToken.EndObject ):
+					if ( objStack.Count > 0 )
+					{
+						objStack.Pop ();
+						objPrefix = GenPrefix ( objStack );
+					}
+					break;
+
+				case ( JsonToken.PropertyName ):
+					token = reader.Value as string;
+					break;
+
+				case ( JsonToken.String ):
+				case ( JsonToken.Integer ):
+				case ( JsonToken.Boolean ):
+				case ( JsonToken.Date ):
+				case ( JsonToken.Bytes ):
+				case ( JsonToken.Float ):
+				case ( JsonToken.Null ):
+				case ( JsonToken.Undefined ):
+					if ( token == null )
+					{
+						throw new ConfigException ( "Malformed config file." );
+					}
+
+					m_tokens.Add ( objPrefix + token, reader.Value );
+					token = null;
+					break;
+				}
+			}
+		}
+
+		public static void Open ( string a_path )
+		{
+			m_instance = new Config ();
+			m_instance._Open ( a_path );
+		}
+
+		public T Get<T> ( string a_token, T a_default = default(T) )
+		{
+			try
+			{
+				return (T)m_tokens[a_token];
+			}
+			catch ( System.Exception )
+			{
+				return a_default;
+			}
+		}
+	}
+}
